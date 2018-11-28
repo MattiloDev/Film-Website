2b80f268703b43d2b6d0e4c2a999646572fa712d
Matthew Hutchings <mwh1g17@soton.ac.uk>
2018-11-28 11:33:28 +0000

    allows multiple quries to execute at the same time

--- includes/database.php
+++ includes/database.php
@@ -5,6 +5,8 @@
 	$db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE,PDO::FETCH_OBJ);
 	
 	$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	
+	PDO::MYSQL_ATTR_USE_BUFFERED_QUERY 
 														
 ?>
 
