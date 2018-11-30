663c031fbe6cfe6ed00d03bbb6ea32fc768b4aa2
Matthew Hutchings <mwh1g17@soton.ac.uk>
2018-11-30 14:42:19 +0000

    Displaying Films on genre page
    Films are now displayed on the genre page however are unclickable and not genre specific

--- genre.php
+++ genre.php
@@ -16,9 +16,37 @@
 
 <div class = "col-9 carosel p-4">
 
+<?php
+
+$filmInfo->execute();
+$genre = $filmInfo->fetchAll();
+// var_dump($genre);
+
+foreach ($genre as $film) { ?> 
+
+    <div class  = "container border-bottom border-white pt-4 pb-4">
+
+    <div class = "row">
+
+    <div class  = "col-2">
+
+        <img src="http://comp2203.ecs.soton.ac.uk/coursework/1617/assets/posters/<?php echo $film->id?>_small.jpg"  height="200" alt="First slide">
+
+    </div>
+
+    <div class = "col-10">
+
+    <h4 class = "marker"> <?php echo $film->name; ?> </h4>
+    <p> <?php echo mb_strimwidth($film->description, 0, 400, "...."); ?> </p>
 
 
+    </div>
 
+    </div>
+
+    </div>
+
+<?php } ?>
 
 <div>
 
